96b02c69d5ee84b7f637478a755a822bb8c044d7
Emilie <emilieho502@gmail.com>
2021-02-09 23:12:52 +0100

    I should not have used React for games, but I added a ball

--- src/components/Ball/Ball.js
+++ src/components/Ball/Ball.js
@@ -1,19 +1,17 @@
-import { makeStyles } from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
+import { makeStyles } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 
 const useStyles = makeStyles({
   gameScreen: {
     height: 10,
     width: 10,
-    background: 'red',
-    borderRadius: '50%',
-    position: 'relative',
-    top: '50%',
+    background: "red",
+    borderRadius: "50%",
+    position: "absolute",
   },
 });
 
-export default function Ball({ position }) {
+export default function Ball({ positionX, positionY }) {
   const classes = useStyles();
-
-  return <div className={classes.gameScreen}></div>;
+  return <div className={classes.gameScreen} style={{ transform: `translate(${positionX}px, ${positionY}px)` }}></div>;
 }
--- src/components/GameScreen/GameScreen.js
+++ src/components/GameScreen/GameScreen.js
@@ -1,29 +1,34 @@
-import { Button, makeStyles } from '@material-ui/core';
-import React, { useEffect, useRef, useState } from 'react';
+import { Button, makeStyles } from "@material-ui/core";
+import { GamepadRounded } from "@material-ui/icons";
+import React, { useEffect, useRef, useState } from "react";
 
 const useStyles = makeStyles({
   gameScreen: {
-    backgroundColor: 'black',
+    backgroundColor: "black",
     width: 1280,
     height: 720,
     //placeSelf: "center",
   },
 });
 
+let i = 1;
+
 export default function GameScreen(props) {
   const [paddlePosition, setPaddlePosition] = useState(0);
+  const [gameStart, setGameStart] = useState(false);
+
+  const [ballProperties, setBallProperties] = useState({});
 
   const classes = useStyles();
 
   const windowRef = useRef();
   const paddleRef = useRef();
-  console.log(props);
+  let mainLoop;
+
   const mouseMove = (e) => {
     const offset = windowRef.current.clientWidth / 2;
     const whitespace = (window.innerWidth - offset * 2) / 2;
     const paddlePosition = e.clientX - whitespace;
-
-    console.log(windowRef);
 
     if (paddlePosition <= paddleRef.current.clientWidth / 2) {
       setPaddlePosition(paddleRef.current.clientWidth / 2 - offset);
@@ -32,13 +37,44 @@ export default function GameScreen(props) {
     } else {
       setPaddlePosition(paddlePosition - offset);
     }
+
+    if (gameStart === false) {
+    }
   };
+
+  const gameLoop = () => {
+    setBallProperties({ positionX: 0, positionY: i });
+    i = i + 5;
+  };
+
+  const pauseGame = () => {
+    clearInterval(mainLoop);
+    setGameStart(false);
+  };
+
+  const startGame = () => {
+    setGameStart(true);
+  };
+
+  useEffect(() => {
+    //if (gameStart) {
+    mainLoop = setInterval(gameLoop, 33);
+    //}
+  }, []);
 
   return (
     <>
       <div ref={windowRef} className={classes.gameScreen} onMouseMove={(e) => mouseMove(e)}></div>
       {props.children[1].type({ mouseX: paddlePosition, paddleRef: paddleRef })}
-      {props.children[0].type({ velocity: 0, direction: 0 })}
+      {
+        //ball
+        props.children[0].type({ positionX: ballProperties.positionX, positionY: ballProperties.positionY })
+      }
+      {gameStart ? (
+        <Button onClick={() => pauseGame()}>Pause</Button>
+      ) : (
+        <Button onClick={() => startGame()}>Start</Button>
+      )}
     </>
   );
 }
